273c977bdb462118437ed0857f24a237a4c25ae9
Grim-redd <mainasara.olulanke@stu.cu.edu.ng>
2023-08-06 17:23:36 +0100

    Fixing up the UI for the Landing Page

--- components/features/Landing-Page/Hero.tsx
+++ components/features/Landing-Page/Hero.tsx
@@ -5,29 +5,23 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="flex flex-col w-full md:h-screen">
-      <div className="h-[78dvh] flex flex-col lg:flex-row items-center bg-[#BDBDBD] px-6 lg:px-8 justify-center pt-9 lg:py-0 pb-6 lg:gap-8">
-        <div className="flex flex-col justify-center h-full gap-5 basis-1/2">
+      <div className="h-[78dvh] flex flex-col lg:flex-row items-center bg-reserved-100 px-6 lg:px-8 justify-center pt-9 lg:py-0 pb-6 lg:gap-8">
+        <div className="flex flex-col justify-center h-full gap-5 text-white basis-1/2">
           <h1 className="text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Manage daily tasks easily anywhere, at all times
           </h1>
-          <p className="w-full text-paragraph">
+          <p className="w-full max-w-[550px] text-paragraph text-reserved-200">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
             convallis suscipit nisi, at congue dolor accumsan id. Donec id
             tempor risus. Etiam cursus risus quis luctus luctus. Curabitur
             tempor nec enim
           </p>
           <div className="flex gap-4">
-            <Link
-              href={"/getStarted"}
-              className="px-8 py-3 bg-white rounded-xs"
-            >
-              Get started
+            <Link href={"/signup"} className="px-8 py-3 rounded-sm bg-primary-40">
+              Post a task for free
             </Link>
-            <Link
-              href={"/getStarted"}
-              className="px-8 py-3 bg-white rounded-xs"
-            >
-              Learn more
+            <Link href={"/signup"} className="px-8 py-3 border rounded-sm">
+              Earn money with ease
             </Link>
           </div>
         </div>
